63a7cf842d3a5db94e9fb7e859cbc7a9c103f934
Wagon <Wagon@WAGON-LAPTOP>
2018-05-24 12:55:36 +0200

    Start with exercise from youtube

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Vidly
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-mint.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
